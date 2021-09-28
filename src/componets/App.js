00262ffbb71b63d8848f31f655bcb41d9b799b60
David Boedigheimer <stealth259@gmail.com>
2021-09-28 15:57:15 -0700

    Add the steps for a static site.

---
+++ src/componets/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <TicketList />
+    </React.Fragment>
+  );
+  {/* this is a JSX comment. */ }
+}
+
+export default App;
